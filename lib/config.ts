20606ec2fa75416586b4d34e2da1b211080dec34
Tomas Gavenciak <gavento@gmail.com>
2025-04-06 20:05:50 +0000

    Add Vasek's draft processed by Gemini

--- lib/config.ts
+++ lib/config.ts
@@ -1,11 +1,19 @@
 import { join } from "path";
 
-export const TITLE = "Generic Academic Website";
-export const DESCRIPTION = "Generic Academic Website";
-export const TITLE_SHORT = "Generic Academic Website";
-export const TITLE_ABBR = "Generic Academic Website";
-export const CHAPTERS = [["Introduction", ""]];
-export const rootSlug = "introduction";
+export const TITLE = "Applied Information Theory: A Computational Toolkit";
+export const DESCRIPTION = "A computational toolkit for applied information theory";
+export const TITLE_SHORT = "Applied Information Theory";
+export const TITLE_ABBR = "Applied Information Theory";
+export const CHAPTERS = [
+  ["Introduction", "index"],
+  ["Quantifying", "01-quantifying"],
+  ["Updating", "02-updating"],
+  ["Modeling", "03-modeling"],
+  ["Advanced", "04-advanced"],
+  ["Resources", "resources"],
+  ["About", "about"],
+];
+export const rootSlug = "index";
 export const contentDirectory = join(process.cwd(), "public");
 export const GTM_ID = undefined;
 export const referencesPath = join(contentDirectory, "references.bib");
