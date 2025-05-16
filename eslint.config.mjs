031df5af95299a8938ae5562de94f01ce8be2981
vaclav rozhon <vaclavrozhon@gmail.com>
2025-05-16 07:02:49 +0200

    i dont know what i am doing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import mdxPlugin from "eslint-plugin-mdx";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +10,22 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
+  // stávající Next.js konfigurace
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // MDX Lint konfigurace
+  {
+    plugins: { mdx: mdxPlugin },
+    overrides: [
+      {
+        files: ["**/*.mdx"],
+        processor: "mdx/mdx",
+        extends: ["plugin:mdx/recommended"],
+        settings: {
+          // umožní lintovat kódové bloky uvnitř trojitých backticků
+          "mdx/code-blocks": true,
+        },
+      },
+    ],
+  },
 ];
-
-export default eslintConfig;
