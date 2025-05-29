c8353bfcc3990f4340d5b249b32c2bf909aa02b1
vaclav rozhon <vaclavrozhon@gmail.com>
2025-05-29 08:56:40 +0200

    shapes in riddle

--- lib/config.ts
+++ lib/config.ts
@@ -1,12 +1,12 @@
 import { join } from "path";
 
-export const TITLE = "Introduction to KL divergence";
-export const DESCRIPTION = "Introduction to KL divergence";
-export const TITLE_SHORT = "Introduction to KL divergence";
-export const TITLE_ABBR = "Introduction to KL divergence";
+export const TITLE = "KL divergence: the good bits";
+export const DESCRIPTION = "KL divergence: the good bits";
+export const TITLE_SHORT = "KL divergence";
+export const TITLE_ABBR = "KL divergence";
 export const CHAPTERS = [
-  ["All those riddles", "00-introduction"],
-  ["KL divergence & Bayes rule", "01-kl_intro"],
+  ["Riddles", "00-introduction"],
+  ["KL divergence & Bayes' rule", "01-kl_intro"],
   ["Crossentropy & Entropy", "02-crossentropy"],
   ["Minimizing KL", "03-minimizing"],
   ["Max entropy distributions", "04-max_entropy"],
